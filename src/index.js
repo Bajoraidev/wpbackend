30a32e1dcccc66e6f06818fabcfbf080cecfe539
NeringaSun <82957315+NeringaSun@users.noreply.github.com>
2021-12-05 18:23:43 +0200

    added reservation post

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const cors = require('cors');
 const { port } = require('./config');
 
 const auth = require('./routes/v1/auth');
+const reservation = require('./routes/v1/reservation');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.use('/v1/auth/', auth);
+app.use('/v1/reservation/', reservation);
 
 app.get('/', (req, res) => {
   res.send({ msg: 'Success' });
---
+++ src/routes/v1/reservation.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+
+const router = express.Router();
+
+const { isLoggedIn } = require('../../middleware');
+const { dbConfig } = require('../../config');
+
+router.post('/', async (req, res) => {
+  try {
+    const con = await mysql.createConnection(dbConfig);
+    const [data] = await con.execute(
+      `INSERT INTO reservation (workplaceid) VALUES (${mysql.escape(
+        req.body.workplaceid,
+      )})`,
+    );
+    await con.end();
+    return res.send(data);
+  } catch (err) {
+    return res.status(500).send({ err: 'error, please try again!' });
+  }
+});
+
+module.exports = router;
