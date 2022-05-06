ac9785807b0b41cdc1f14cb6a604a1d3572ee7b3
NeringaSun <82957315+NeringaSun@users.noreply.github.com>
2022-05-06 10:26:23 +0300

    updates

--- src/routes/v1/reservation.js
+++ src/routes/v1/reservation.js
@@ -56,7 +56,7 @@ router.get('/list/:data?', async (req, res) => {
 */
 
 router.get('/wp/:wp?', async (req, res) => {
-  const query = `SELECT workplaceid, res_day, user_name from reservation ${
+  const query = `SELECT workplaceid from reservation ${
     req.params.data && `WHERE workplaceid = '${req.params.wp}'`
   }
   `;
